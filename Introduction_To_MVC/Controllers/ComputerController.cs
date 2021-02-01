2ce342d218086238e779c836f55f4f4e78db2e98
EsraaMegdadi <41356926+EsraaMegdadi@users.noreply.github.com>
2021-02-01 02:09:33 +0200

    Add project file, create MVC computer project and update some changes on _layout view ( lesson #3 & #4 )

---
+++ Introduction_To_MVC/Controllers/ComputerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Introduction_To_MVC.Models;
+
+namespace Introduction_To_MVC.Controllers
+{
+    public class ComputerController : Controller
+    {
+        // GET: Computer
+        public ActionResult Display()
+        {
+            var myComputer = new Computer
+            {
+                Name = "lenovo",
+                price = 1570
+            };
+            return View(myComputer);
+        }
+    }
+}
---
+++ Introduction_To_MVC/Models/Computer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Introduction_To_MVC.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double price  { get; set; }
+    }
+}
