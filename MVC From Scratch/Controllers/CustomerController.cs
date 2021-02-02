71ef8df70553e29f36a56b6f8ebd1bd149b34bbb
EsraaMegdadi <41356926+EsraaMegdadi@users.noreply.github.com>
2021-02-02 22:26:08 +0200

    Lesson8 [ Razor MVC ]

---
+++ MVC From Scratch/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_From_Scratch.Models;
+
+namespace MVC_From_Scratch.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult DisplayCustomer ()
+        {
+            var customer = new Customer(1,"Esraa Megdadi","Irbid",25);
+            return View(customer);
+        }
+    }
+}
