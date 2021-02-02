0501ddea70b7edbeb53d32f37f3cd51c24f24820
EsraaMegdadi <41356926+EsraaMegdadi@users.noreply.github.com>
2021-02-02 12:04:39 +0200

    lesson6 [RoutePrefix on Attribute-Based Routing]

---
+++ Introduction_To_MVC/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Introduction_To_MVC.Controllers
+{
+    //RoutePrefix
+    [RoutePrefix("Student")]
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [Route("Details/{id:int}")]
+        public ActionResult Details(int id)
+        {
+            return Content("<h3>Student ID is </h3><h1>" + id + "</h1>");
+        }
+
+        
+        [Route("Details/{name}")]
+        public ActionResult Details(string name)
+        {
+            return Content("<h3>Student name is </h3>" + name);
+
+        }
+    }
+}
